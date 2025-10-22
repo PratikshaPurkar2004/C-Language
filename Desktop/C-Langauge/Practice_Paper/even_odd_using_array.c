d4c19777aeaf57f4cf6bfdfffe6572f201ca5a33
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2025-10-22 23:16:53 +0530

    22-10-2025

--- Desktop/C-Langauge/Practice_Paper/even_odd_using_array.c
+++ Desktop/C-Langauge/Practice_Paper/even_odd_using_array.c
@@ -16,14 +16,32 @@ int main()
     printf("enter array elements:");
     for(i=0;i<n;i++)
         scanf("%d",&A[i]);
-
+    int evencnt=0,oddcnt=0;
     for(i=0;i<n;i++)
     {
         if(A[i]%2==0)
+        {
+            A[i]=0;
+            evencnt++;
+        }
+        else
+        {
+            A[i]=1;
+            oddcnt++;
+        }
+
+    }
+    printf("even count =%d\n odd count=%d\n",evencnt,oddcnt);
+    for(i=0;i<n;i++)
+    {
+        if(i<evencnt)
             A[i]=0;
         else
             A[i]=1;
     }
+    for(i=0;i<n;i++)
+        printf("%d ",A[i]);
+
     free(A);
     return 0;
 }
