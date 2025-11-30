3bb14b0af845320e6f4bfe5c7903f8ecc8113676
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2025-11-30 21:38:20 +0530

    30-11-2025

---
+++ Desktop/C-Langauge/Function/factorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int factorial(int n);
+int main()
+{
+    int n,fact;
+    printf("enter an num::");
+    scanf("%d",&n);
+    fact=factorial(n);
+    printf("%d",fact);
+    return 0;
+}
+
+int factorial(int n)
+{
+    int fact=1;
+    for(int i=1;i<=n;i++)
+        fact=fact*i; 
+    return fact;  
+}
