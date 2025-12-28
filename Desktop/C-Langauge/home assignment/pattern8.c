8cc29e9f8516d4a2d71d326726979edffb794f3a
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2025-12-28 20:31:27 +0530

    28-12-2025

---
+++ Desktop/C-Langauge/home assignment/pattern8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("enter a num::");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if((i+j)%2==0)
+                printf("1\t");
+            else
+                printf("0\t");
+        }
+        printf("\n");
+    }
+    return 0;
+}
