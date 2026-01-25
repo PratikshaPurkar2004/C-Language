a31198d677ad544c905d82e043e328e8c7bdbf06
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-01-25 20:57:03 +0530

    25-01-2026

---
+++ Desktop/C-Langauge/home assignment/pattern_num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("enter a num::");
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        for(j=1;j<n;j++)
+            printf("%d",i);
+        printf("\n");
+    }
+    return 0;
+}
